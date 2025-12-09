b93419a41113850c43d7173d70cd8efbb88143f6
Lilith-11 <lilithkcse2021@gmail.com>
2025-12-09 21:56:58 +0530

    sum of integer

---
+++ sum_two_numbers.go
@@ -0,0 +1,11 @@
+package main
+import "fmt"
+func main(){
+	var a,b int
+	fmt.Print("enter your first number:")
+	fmt.Scan(&a)
+	fmt.Print("12enter your second number:")
+    fmt.Scan(&b)
+	fmt.Println("sum of two number is",a+b)
+
+}
